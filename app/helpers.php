19db4d8372450f6e3d66658cc94feefa5318c1ea
chengyao <64066545+topyao@users.noreply.github.com>
2022-06-03 19:36:36 +0800

    [pref] 添加助手函数

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,32 @@
 <?php
+
+use Max\Config\Repository;
+use Max\Di\Context;
+use Max\Utils\Arr;
+use Psr\Container\ContainerExceptionInterface;
+
+if (false === function_exists('config')) {
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws ReflectionException
+     */
+    function config(string $key, $default = null): mixed
+    {
+        /** @var Repository $config */
+        $config = Context::getContainer()->make(Repository::class);
+        return $config->get($key, $default);
+    }
+}
+
+if (false === function_exists('env')) {
+    /**
+     * @param string $key
+     * @param        $default
+     *
+     * @return array|ArrayAccess|mixed
+     */
+    function env(string $key, $default = null): mixed
+    {
+        return Arr::get($_ENV, strtoupper($key), $default);
+    }
+}
