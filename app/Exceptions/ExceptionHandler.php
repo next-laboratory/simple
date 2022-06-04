7b3bbc82c584cc92be87d4e8a58b399f9d4e7a43
chengyao <64066545+topyao@users.noreply.github.com>
2022-06-04 18:07:31 +0800

    [pref] 优化异常处理

--- app/Exceptions/ExceptionHandler.php
+++ app/Exceptions/ExceptionHandler.php
@@ -14,39 +14,7 @@ declare(strict_types=1);
 namespace App\Exceptions;
 
 use Max\HttpServer\ExceptionHandler as HttpExceptionHandler;
-use Psr\Http\Message\ServerRequestInterface;
-use Throwable;
 
 class ExceptionHandler extends HttpExceptionHandler
 {
-    /**
-     * @param Throwable              $throwable
-     * @param ServerRequestInterface $request
-     *
-     * @return void
-     */
-    protected function reportException(Throwable $throwable, ServerRequestInterface $request): void
-    {
-        if (PHP_SAPI === 'cli') {
-            $this->dumpException($throwable);
-        } else {
-            echo $throwable->getMessage();
-        }
-    }
-
-    /**
-     * @param Throwable $throwable
-     *
-     * @return void
-     */
-    protected function dumpException(Throwable $throwable): void
-    {
-        echo sprintf("[%s] %s in %s +%d\n%s\n",
-            $throwable::class,
-            $throwable->getMessage(),
-            $throwable->getFile(),
-            $throwable->getLine(),
-            $throwable->getTraceAsString()
-        );
-    }
 }
