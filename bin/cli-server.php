4e61da488a9bc2f255af835a7b913b50ce134d0b
chengyao <64066545+topyao@users.noreply.github.com>
2022-06-09 19:13:03 +0800

    [pref] 优化代码

---
+++ bin/cli-server.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Max package.
+ *
+ * (c) Cheng Yao <987861463@qq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$host = '0.0.0.0';
+$port = 8989;
+echo <<<EOT
+,--.   ,--.                  ,------. ,--.  ,--.,------.  
+|   `.'   | ,--,--.,--.  ,--.|  .--. '|  '--'  ||  .--. ' 
+|  |'.'|  |' ,-.  | \  `'  / |  '--' ||  .--.  ||  '--' | 
+|  |   |  |\ '-'  | /  /.  \ |  | --' |  |  |  ||  | --'  
+`--'   `--' `--`--''--'  '--'`--'     `--'  `--'`--' 
+
+EOT;
+printf("System       Name:       %s\n", strtolower(PHP_OS));
+printf("PHP          Version:    %s\n", PHP_VERSION);
+passthru('php -S ' . $host . ':' . $port . ' -t public/ server.php');
