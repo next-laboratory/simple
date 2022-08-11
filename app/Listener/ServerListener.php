96f0e48e417a0d934db03bdc4eb364e132bc7215
chengyao <bigyao@139.com>
2022-08-11 20:28:54 +0800

    Add: 添加请求时间统计支持

--- app/Listener/ServerListener.php
+++ app/Listener/ServerListener.php
@@ -30,7 +30,7 @@ class ServerListener implements EventListenerInterface
         if ($event instanceof OnRequest) {
             $statusCode = $event->response->getStatusCode();
             $statusCode = ($statusCode == 200 ? "\033[42m" : "\033[41m") . $statusCode . "\033[0m";
-            echo sprintf("[MaxPHP] %s |%s| %15s|\033[44m%7s\033[0m| \"%s\"\n", date('Y/m/d H:i:s'), $statusCode, $event->request->getRealIp(), $event->request->getMethod(), $event->request->getUri()->getPath());
+            echo sprintf("[MaxPHP] %s |%s|%10.3fms| %15s|\033[44m%7s\033[0m| \"%s\"\n", date('Y/m/d H:i:s'), $statusCode, (microtime(true) - $event->requestedAt) * 1000, $event->request->getRealIp(), $event->request->getMethod(), $event->request->getUri()->getPath());
         }
     }
 }
