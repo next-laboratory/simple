6d2895ac3ae775d7e20d153a6b2bde547ea06225
chengyao <64066545+topyao@users.noreply.github.com>
2022-08-12 20:49:07 +0800

    Pref: Code wrap

--- app/Listener/ServerListener.php
+++ app/Listener/ServerListener.php
@@ -28,9 +28,16 @@ class ServerListener implements EventListenerInterface
     public function process(object $event): void
     {
         if ($event instanceof OnRequest) {
+            $request    = $event->request;
             $statusCode = $event->response->getStatusCode();
-            $statusCode = ($statusCode == 200 ? "\033[42m" : "\033[41m") . $statusCode . "\033[0m";
-            echo sprintf("[MaxPHP] %s |%s|%10.3fms| %15s|\033[44m%7s\033[0m| \"%s\"\n", date('Y/m/d H:i:s'), $statusCode, (microtime(true) - $event->requestedAt) * 1000, $event->request->getRealIp(), $event->request->getMethod(), $event->request->getUri()->getPath());
+            echo sprintf("[MaxPHP] %s |%s|%10.3fms| %15s|\033[44m%7s\033[0m| \"%s\"\n",
+                date('Y/m/d H:i:s'),
+                ($statusCode == 200 ? "\033[42m" : "\033[41m") . $statusCode . "\033[0m",
+                (microtime(true) - $event->requestedAt) * 1000,
+                $request->getRealIp(),
+                $request->getMethod(),
+                $request->getUri()->getPath()
+            );
         }
     }
 }
