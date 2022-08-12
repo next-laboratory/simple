35d7dab2f584feb02cd8de72bb9ead1f25bea248
chengyao <64066545+topyao@users.noreply.github.com>
2022-08-12 09:32:17 +0800

    Pref: database 优化

--- app/Listener/DatabaseQueryListener.php
+++ app/Listener/DatabaseQueryListener.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
 
 namespace App\Listener;
 
-use Max\Database\Events\QueryExecuted;
+use Max\Database\Event\QueryExecuted;
 use Max\Event\Contract\EventListenerInterface;
 use Psr\Log\LoggerInterface;
 
