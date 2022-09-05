4effc5e97a6302d8f65d6f324f63baeebcdd6ee3
chengyao <bigyao@139.com>
2022-09-05 18:50:02 +0800

    Add: test

---
+++ test/Cases/ExampleCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test\Cases;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleCase extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
