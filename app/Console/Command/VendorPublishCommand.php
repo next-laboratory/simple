4d9c07bd4f7d1373bccef14103b09659c7299e39
chengyao <bigyao@139.com>
2022-09-10 22:47:13 +0800

    Fix：vendor publish

--- app/Console/Command/VendorPublishCommand.php
+++ app/Console/Command/VendorPublishCommand.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
 
 namespace App\Console\Command;
 
-use Max\Aop\Scanner;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -21,22 +20,36 @@ class VendorPublishCommand extends Command
     protected function configure()
     {
         $this->setName('vendor:publish')
-            ->setDescription('Publish publishable packages');
+             ->setDescription('Publish publishable packages');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $path   = getcwd();
-        $config = Scanner::scanConfig($path . '/vendor/composer/installed.json');
-        if (isset($config['publish'])) {
-            foreach ($config['publish'] as $publish) {
-                $destination = $publish['destination'];
-                if (! file_exists($destination)) {
-                    copy($publish['source'], $publish['destination']);
-                    $output->writeln('<info>[DEBUG]</info> Package `' . $publish['name'] . '` config file published.');
+        $path      = getcwd();
+        $installed = json_decode(file_get_contents($path . '/vendor/composer/installed.json'), true);
+        $installed = $installed['packages'] ?? $installed;
+        $config    = [];
+        foreach ($installed as $package) {
+            if (isset($package['extra']['max']['config'])) {
+                $configProvider = $package['extra']['max']['config'];
+                $configProvider = new $configProvider();
+                if (method_exists($configProvider, '__invoke')) {
+                    if (is_array($configItem = $configProvider())) {
+                        $config = array_merge_recursive($config, $configItem);
+                    }
                 }
             }
         }
+
+//        if (isset($config['publish'])) {
+//            foreach ($config['publish'] as $publish) {
+//                $destination = $publish['destination'];
+//                if (!file_exists($destination)) {
+//                    copy($publish['source'], $publish['destination']);
+//                    $output->writeln('<info>[DEBUG]</info> Package `' . $publish['name'] . '` config file published.');
+//                }
+//            }
+//        }
         $path .= '/runtime/app/';
         file_exists($path) || mkdir($path);
         file_put_contents($path . 'config.php', sprintf("<?php\n\nreturn %s;", var_export($config, true)));
